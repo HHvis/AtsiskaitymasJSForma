0e81cc1fa553c1962003542ce9c1fb3269c07a7f
Edvin <95039110+HHvis@users.noreply.github.com>
2022-03-02 12:53:16 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+let vardas = document.getElementById("vardas");
+let pavarde = document.getElementById("pavarde");
+let mokykla = document.getElementById("mokykla");
+let miestas = document.getElementById("miestas");
+let sritis = document.getElementById("sritis");
+
+let registracija = document.getElementById("registruotis");
+let spausdinti = document.getElementById("btn-list");
+
+let list = document.getElementById("list");
+
+
+registracija.addEventListener("click", function(){
+    const notEmpty = str => !str.trim().length;
+    
+    if( notEmpty(vardas.value) ) {
+        alert( "Uzpildykite forma" )
+    } else if ( notEmpty(pavarde.value) ) {
+        alert( "Uzpildykite forma" )
+    } else if ( notEmpty(mokykla.value) ) {
+        alert( "Uzpildykite forma" )
+    } else if ( notEmpty(miestas.value) ) {
+        alert( "Uzpildykite forma" )
+    } else if(sritis.value == "--Pasirinkti--"){
+        alert("Pasirink sriti");
+    }
+    else {
+    list.innerHTML += vardas.value + ", "+ pavarde.value + ", "+ mokykla.value + ", "+ miestas.value + ", sritis: " + sritis.value;
+    }
+});
+
+spausdinti.addEventListener("click", function(){
+    list.style.display = "block";
+});
